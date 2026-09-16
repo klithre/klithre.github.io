089fc276f9e11a5f425738e8b92da2198dc2ed03
klithre <5129911@opsb.info>
2026-09-16 16:26:24 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,25 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    //toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+createPlatform(50, 400, 150, 100, "black");
+createPlatform(250, 320, 100, 100, "red");
+createPlatform(450, 340, 100, 100, "green");
+createPlatform(700, 400, 150, 100, "blue");
+createPlatform(950, 500, 170, 100, "purple"); // purple for a finished platform 
 
     // TODO 3 - Create Collectables
+createCollectable("diamond", 1000, 450);
+createCollectable("max", 750, 300);
+createCollectable("grace", 350, 200)
 
-
-
-    
     // TODO 4 - Create Cannons
+createCannon("right", 750, 100);
+createCannon("left", 20, 100); 
+createCannon("bottom", 1000, 100);
 
 
     
